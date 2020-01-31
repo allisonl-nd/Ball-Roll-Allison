49793ce8c4773e3a9c455b34d4d22fe46765382b
Kameron Heyen <kameronh7@gmail.com>
2020-01-31 16:26:51 -0600

    Module 2.0

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PlayerController : MonoBehaviour {
+public class BallController : MonoBehaviour {
 
     public float speed;
     public Text countText;
@@ -24,15 +24,6 @@ public class PlayerController : MonoBehaviour {
         audio = GetComponent<AudioSource>();
     }
 
-    void FixedUpdate ()
-    {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-
-        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-
-        rb.AddForce (movement * speed);
-    }
 
     void OnTriggerEnter(Collider other)
     {
@@ -48,7 +39,7 @@ public class PlayerController : MonoBehaviour {
     void SetCountText ()
     {
         countText.text = "Count: " + count.ToString ();
-        if (count >= 13)
+        if (count >= 8)
         {
             winText.text = "You Win!";
         }
